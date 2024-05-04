cea39541f0177c8d32f4db1426d97ff0514d5165
Anastasiia Selivanova <selivanovaana5tacia@yandex.ru>
2024-05-05 02:23:51 +0500

    feat: add singleton

---
+++ patterns/1-creational/4-singleton.js
@@ -0,0 +1,24 @@
+// Например работа с базой данных (только одно подключение)
+
+class Database {
+
+  constructor(data) {
+    if (Database.exists) {
+      return Database.instance
+    }
+
+    Database.instance = this
+    Database.exists = true
+    this.data = data
+  }
+
+  getData () {
+    return this.data
+}
+}
+
+const mongo = new Database('MongoDB')
+console.log(mongo.getData()) //'MongoDB'
+
+const mysql = new Database('MySQL')
+console.log(mongo.getData()) //'MongoDB'
