02ac1d33c64f9e528307bd09697a3b5e4e318aa8
Anastasiia Selivanova <selivanovaana5tacia@yandex.ru>
2024-07-09 00:40:47 +0500

    feat: add popup-plugin part2

--- popup-plugin/index.js
+++ popup-plugin/index.js
@@ -1,7 +1,7 @@
 const fruits = [
-  {id:1, title: 'Apples', price: 20, img: ''},
-  {id:1, title: 'Oranges', price: 12, img: ''},
-  {id:1, title: 'Bananas', price: 15, img: ''},
+  {id:1, title: 'Apples', price: 20, img: 'https://media.istockphoto.com/id/184276818/ru/%D1%84%D0%BE%D1%82%D0%BE/%D0%BA%D1%80%D0%B0%D1%81%D0%BD%D0%BE%D0%B5-%D1%8F%D0%B1%D0%BB%D0%BE%D0%BA%D0%BE.jpg?s=612x612&w=0&k=20&c=HDH3wLEAvc7soT85pAcS4JOQu5KJ8xM9JOilVe1zFLI='},
+  {id:1, title: 'Oranges', price: 12, img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVHePIe0HcJfnuXHoct2eH9_qwdKU9cWli4g&s'},
+  {id:1, title: 'Bananas', price: 15, img: 'https://lenta.servicecdn.ru/globalassets/1/-/01/12/30/4dc5744e-8d20-5ef3-bac9-9e2d1fd3f6e7.png?preset=fulllossywhite'},
 ]
 
 const modal = $.modal({
@@ -12,5 +12,15 @@ const modal = $.modal({
   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
   quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo</p>
   `,
-  width: '400px'
+  width: '400px',
+  footerButtons: [
+    {text: 'Ok', type: 'primary', handler() {
+      console.log('primary btn clicked')
+        modal.close()
+      }},
+    {text: 'Cancel', type: 'danger', handler() {
+        console.log('Cancel btn clicked')
+        modal.close()
+      }},
+  ]
 })
--- popup-plugin/plugins/modal.js
+++ popup-plugin/plugins/modal.js
@@ -1,24 +1,49 @@
+Element.prototype.appendAfter = function(element) {
+  element.parentNode.insertBefore(this, element.nextSibling)
+}
+
+function noop() {}
+
+function _createModalFooter(buttons = []) {
+  if (buttons.length === 0) {
+    return document.createElement('div')
+  }
+
+  const wrap  = document.createElement('div')
+  wrap.classList.add('modal-footer')
+
+  buttons.forEach(button => {
+    const $btn = document.createElement('button')
+    $btn.textContent = button.text
+    $btn.classList.add('btn')
+    $btn.classList.add(`btn-${button.type || 'secondary'}`)
+    $btn.onclick = button.handler || noop
+
+    wrap.appendChild($btn)
+  })
+
+  return wrap
+}
+
 function _createModal(options) {
+  const DEFAULT_WIDTH = '600px'
   const modal = document.createElement('div')
   modal.classList.add('vmodal')
   modal.insertAdjacentHTML('afterbegin', `
-    <div class="modal-overlay">
-        <div class="modal-window">
+    <div class="modal-overlay" data-close="true">
+        <div class="modal-window" style="width: ${options.width || DEFAULT_WIDTH}" >
             <div class="modal-header">
-                <span class="modal-title">Modal Title</span>
-                <span class="modal-close">&times;</span>
+                <span class="modal-title">${options.title || 'Default Title'}</span>
+                ${options.closable ? `<span class="modal-close" data-close="true">&times;</span>` : ' '}
             </div>
-            <div class="modal-body">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt</p>
-            </div>
-            <div class="modal-footer">
-                <button>Ok</button>
-                <button>Cancel</button>
+            <div class="modal-body" data-content>
+                ${options.content || ' '}
             </div>
         </div>
     </div>
 `)
+  const footer = _createModalFooter(options.footerButtons)
+  footer.appendAfter(modal.querySelector('[data-content]'))
   document.body.appendChild(modal)
   return modal
 }
@@ -27,9 +52,13 @@ $.modal = function(options) {
   const ANIMATION_SPEED = 200
   const $modal = _createModal(options)
   let closing = false
+  let destroyed = false
 
-  return {
+  const modal = {
     open() {
+      if (destroyed) {
+        return console.log('Modal is destroyed')
+      }
       !closing && $modal.classList.add('open')
     },
     close() {
@@ -41,6 +70,26 @@ $.modal = function(options) {
         closing = false
       }, ANIMATION_SPEED)
     },
-    destroy() {},
   }
+
+  const listener = event => {
+    if (event.target.dataset.close) {
+      modal.close()
+    }
+  }
+
+
+  $modal.addEventListener('click', listener)
+
+  return Object.assign(modal, {
+    destroy() {
+      $modal.parentNode.replaceChild($modal)
+      $modal.removeEventListener('click', listener)
+      destroyed = true
+    },
+    setContent(html) {
+      $modal.querySelector('[data-content]').innerHTML = html
+    }
+
+  })
 }
